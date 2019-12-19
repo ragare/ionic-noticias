8d01cdaa506b117bb194e6898ea0a7cd38912fbb
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2019-12-19 16:28:31 +0100

    Componentes individuales para las noticias y la noticia

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { NoticiaComponent } from './noticia/noticia.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NoticiasComponent,
+    NoticiaComponent
+  ],
+  exports: [
+    NoticiasComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ]
 })
 export class ComponentsModule { }
--- src/app/components/noticias/noticias.component.ts
+++ src/app/components/noticias/noticias.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-noticias',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./noticias.component.scss'],
 })
 export class NoticiasComponent implements OnInit {
+
+  @Input() noticias: Article[] = [];
 
   constructor() { }
 
